e069aa5aee9c79fe8563c8a742a8249eb7d9c34b
Luke Fisher <lukewfisher@msn.com>
2018-10-14 21:30:08 -0600

    some verbeage cleanup and an attempt at the game API

--- server/routing/game.go
+++ server/routing/game.go
@@ -3,6 +3,7 @@ package routing
 import (
 	"net/http"
 
+	"github.com/bitdecaygames/fireport/server/services"
 	"github.com/gorilla/mux"
 )
 
@@ -12,13 +13,27 @@ const (
 
 // GameRoutes contains information about routes specific to game interaction
 type GameRoutes struct {
+	Service services.GameService
 }
 
 // AddRoutes will add all game routes to the given router
 func (gr *GameRoutes) AddRoutes(r *mux.Router) {
+	r.HandleFunc(gameRoute, gr.gameCreateHandler).Methods("POST")
 	r.HandleFunc(gameRoute+"/{gameName}/tick/{tick}/player/{playerName}/cards", gr.submitCardsHandler).Methods("PUT")
 	r.HandleFunc(gameRoute+"/{gameName}/tick", gr.getCurrentTickHandler).Methods("GET")
 	r.HandleFunc(gameRoute+"/{gameName}/tick/{tick}/player/{playerName}", gr.getGameStateHandler).Methods("GET")
+}
+
+func (gr *GameRoutes) gameCreateHandler(w http.ResponseWriter, r *http.Request) {
+	panic("Not yet implemented")
+	// decoder := json.NewDecoder(r.Body)
+	// var t string
+	// err := decoder.Decode(&t)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// game := gr.Service.CreateGame(t)
+	// w.Write([]byte(game.ID.String()))
 }
 
 // submitCardsHandler handles a player request to submit cards for the given tick
