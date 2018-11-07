5e77ea40fb039687876d241df65e0ab58c5eedec
Logan <monday.hopscotch@gmail.com>
2018-11-06 19:44:48 -0700

    stand-in for sending out updated state

--- server/services/game_service.go
+++ server/services/game_service.go
@@ -100,6 +100,13 @@ func (g *GameServiceImpl) SubmitTurn(submit pogo.TurnSubmissionMsg) error {
 
 	if allTurnsSubmitted {
 		// TODO: step the game
+
+		for pid, conn := range game.ActiveConnections {
+			err = conn.WriteJSON(game)
+			if err != nil {
+				fmt.Println("failed to send state to player ", pid)
+			}
+		}
 	}
 
 	return nil
