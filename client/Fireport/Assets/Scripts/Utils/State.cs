46b44370f9a860f1f65dd8f2a610cb55a4d0408c
mwingfield <mwingfie@gmail.com>
2018-11-14 18:42:29 -0700

    fixes #63 set up https for server

--- client/Fireport/Assets/Scripts/Utils/State.cs
+++ client/Fireport/Assets/Scripts/Utils/State.cs
@@ -1,7 +1,7 @@
 namespace Utils {
     public static class State {
-        public const string HTTP_HOST = "http://ec2-34-217-206-186.us-west-2.compute.amazonaws.com:8080";
-        public const string WEBSOCKET_HOST = "ws://ec2-34-217-206-186.us-west-2.compute.amazonaws.com:8080";
+        public const string HTTP_HOST = "https://tanxz.bytebreakstudios.com";
+        public const string WEBSOCKET_HOST = "ws://tanxz.bytebreakstudios.com:8080"; // TODO: MW this might be a problem with iOS because it is not a secure connection
 
         // for localhost testing
 //        public const string HTTP_HOST = "http://localhost:8080";
