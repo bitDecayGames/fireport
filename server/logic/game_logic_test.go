2dd5a0c849806831c7b26658af28222403d3d642
Logan <monday.hopscotch@gmail.com>
2018-11-20 16:45:42 -0700

    update tests. DRY up getting test state

--- server/logic/game_logic_test.go
+++ server/logic/game_logic_test.go
@@ -1,6 +1,7 @@
 package logic
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/bitdecaygames/fireport/server/pogo"
@@ -8,7 +9,20 @@ import (
 )
 
 func TestIncrementTurn(t *testing.T) {
-	curState := &pogo.GameState{Players: []pogo.PlayerState{{ID: 0, Hand: []pogo.CardState{{ID: 1, CardType: pogo.SkipTurn}}}}}
+	curState := &pogo.GameState{
+		RNG: rand.New(rand.NewSource(0)),
+		Players: []pogo.PlayerState{
+			{
+				ID: 0,
+				Hand: []pogo.CardState{
+					{
+						ID:       1,
+						CardType: pogo.SkipTurn,
+					},
+				},
+			},
+		},
+	}
 	// this will actually increment the turn by 2 because of the DefaultTurnActions list being applied at the end
 	nextState, err := StepGame(curState, []pogo.GameInputMsg{{CardID: 1, Owner: 0}})
 
--- server/triggers/triggers_test.go
+++ server/triggers/triggers_test.go
@@ -9,82 +9,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func getTestState() *pogo.GameState {
-	return &pogo.GameState{
-		Turn:        0,
-		Created:     1000,
-		Updated:     2000,
-		IDCounter:   300,
-		BoardWidth:  2,
-		BoardHeight: 2,
-		BoardSpaces: []pogo.BoardSpace{
-			{ID: 0, SpaceType: 0, State: 0},
-			{ID: 1, SpaceType: 0, State: 0},
-			{ID: 2, SpaceType: 0, State: 0},
-			{ID: 3, SpaceType: 0, State: 0},
-		},
-		Players: []pogo.PlayerState{
-			{
-				ID:       100,
-				Name:     "PlayerOne",
-				Location: 0,
-				Health:   10,
-				Hand: []pogo.CardState{
-					{ID: 101, CardType: 0},
-					{ID: 102, CardType: 0},
-					{ID: 103, CardType: 0},
-					{ID: 104, CardType: 0},
-					{ID: 105, CardType: 0},
-				},
-				Deck: []pogo.CardState{
-					{ID: 106, CardType: 0},
-					{ID: 107, CardType: 0},
-					{ID: 108, CardType: 0},
-					{ID: 109, CardType: 0},
-					{ID: 110, CardType: 0},
-				},
-				Discard: []pogo.CardState{
-					{ID: 111, CardType: 0},
-					{ID: 112, CardType: 0},
-					{ID: 113, CardType: 0},
-					{ID: 114, CardType: 0},
-					{ID: 115, CardType: 0},
-				},
-			},
-			{
-				ID:       200,
-				Name:     "PlayerTwo",
-				Location: 3,
-				Health:   10,
-				Hand: []pogo.CardState{
-					{ID: 201, CardType: 0},
-					{ID: 202, CardType: 0},
-					{ID: 203, CardType: 0},
-					{ID: 204, CardType: 0},
-					{ID: 205, CardType: 0},
-				},
-				Deck: []pogo.CardState{
-					{ID: 206, CardType: 0},
-					{ID: 207, CardType: 0},
-					{ID: 208, CardType: 0},
-					{ID: 209, CardType: 0},
-					{ID: 210, CardType: 0},
-				},
-				Discard: []pogo.CardState{
-					{ID: 211, CardType: 0},
-					{ID: 212, CardType: 0},
-					{ID: 213, CardType: 0},
-					{ID: 214, CardType: 0},
-					{ID: 215, CardType: 0},
-				},
-			},
-		},
-	}
-}
-
 func TestWinTrigger(t *testing.T) {
 	var trigger = &WinTrigger{}
-	var a = getTestState()
+	var a = pogo.GetTestState()
 
 	assert.Equal(t, 0, a.Winner)
 	assert.Equal(t, false, a.IsGameFinished)
@@ -115,7 +42,7 @@ func TestWinTrigger(t *testing.T) {
 func TestNotEnoughCardsInHandTrigger(t *testing.T) {
 	var trigger = &NotEnoughCardsInHandTrigger{RequiredCardsInHand: 5}
 	var rule = &rules.CardIdsCannotBeChangedRule{}
-	var a = getTestState()
+	var a = pogo.GetTestState()
 
 	var b, err = ApplyTriggers(a, []Trigger{trigger})
 	assert.NoError(t, err)
@@ -143,7 +70,7 @@ func TestNotEnoughCardsInHandTrigger(t *testing.T) {
 func TestRefreshDeckTrigger(t *testing.T) {
 	var trigger = &RefreshDeckTrigger{}
 	var rule = &rules.CardIdsCannotBeChangedRule{}
-	var a = getTestState()
+	var a = pogo.GetTestState()
 
 	var b, err = ApplyTriggers(a, []Trigger{trigger})
 	assert.NoError(t, err)
@@ -191,7 +118,7 @@ func TestRefreshDeckTrigger(t *testing.T) {
 func TestDrawAndDeckRefreshTriggersTogether(t *testing.T) {
 	var triggers = []Trigger{&NotEnoughCardsInHandTrigger{RequiredCardsInHand: 5}, &RefreshDeckTrigger{}}
 	var rule = &rules.CardIdsCannotBeChangedRule{}
-	var a = getTestState()
+	var a = pogo.GetTestState()
 
 	var b, err = ApplyTriggers(a, triggers)
 	assert.NoError(t, err)
@@ -231,7 +158,7 @@ func TestDrawAndDeckRefreshTriggersTogether(t *testing.T) {
 }
 
 func TestDiscardUsedCardsTrigger(t *testing.T) {
-	var a = getTestState()
+	var a = pogo.GetTestState()
 
 	var b, err = ApplyTriggers(a, []Trigger{&DiscardUsedCardsTrigger{Cards: []int{101, 201, 202}}})
 	var rule = &rules.CardIdsCannotBeChangedRule{}
