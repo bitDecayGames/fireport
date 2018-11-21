d4806691948a8e3dabf14acc21c6e8dfdfcc863e
Logan <monday.hopscotch@gmail.com>
2018-11-20 20:38:18 -0700

    update ssh config names to be accurate to documentation

--- server/deploy.sh
+++ server/deploy.sh
@@ -3,7 +3,7 @@ env GOOS=linux GOARCH=amd64 go build main.go
 echo "SUCCESS"
 
 echo "Scp main executable to server"
-scp main ec2-34-217-206-186.us-west-2.compute.amazonaws.com:
+scp main fireport01_ubuntu:
 echo "SUCCESS"
 
 echo "Move to ansible directory and start setup playbook"
