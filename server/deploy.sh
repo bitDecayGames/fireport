62c43e1bcc82a90ecf9b2ae9fd188a6bbfefb474
mwingfield <mwingfie@gmail.com>
2019-01-05 09:15:30 -0700

    yay, deploy now takes 30 seconds

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,12 +1,18 @@
 echo "Build latest Go server"
 env GOOS=linux GOARCH=amd64 go build main.go
-echo "SUCCESS"
+
+echo "Dockerize the binary"
+docker build -t bitdecaygames/fireport:latest .
+
+echo "Tarball the docker image"
+docker save -o ./fireport-container.tar bitdecaygames/fireport:latest
 
 echo "Scp main executable to server"
-scp main fireport01_ubuntu:
-echo "SUCCESS"
+scp fireport-container.tar fireport01_ubuntu:
 
 echo "Move to ansible directory and start setup playbook"
 cd ansible/
 ansible-playbook -i dev setup.yml
-echo "SUCCESS"
+
+echo "Run deploy playbook"
+ansible-playbook -i dev deploy.yml
