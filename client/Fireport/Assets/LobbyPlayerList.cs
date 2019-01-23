d6761cec60ae536c1fd76b1034c386f54824c568
Logan <monday.hopscotch@gmail.com>
2019-01-22 19:25:29 -0700

    initial good stuff

---
+++ client/Fireport/Assets/LobbyPlayerList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Model;
+using Model.Message;
+
+public class LobbyPlayerList : MonoBehaviour
+{
+	public GameObject playerRowPrefab;
+
+	void UpdateLobbyPlayers(LobbyMessage msg)
+	{
+		
+	}
+}
