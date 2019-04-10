fdd9a5652f88d9d155da83129f7792e3a09c7349
U-DESKTOP-ADQ4UE8\fishe <zombiefish90@gmail.com>
2019-04-09 20:37:39 -0600

    Game over scene fixingingin 	modified:   client/Fireport/Assets/Scenes/GameOverScene.unity 	modified:   client/Fireport/Assets/Scripts/Game/GameManagerBehaviour.cs 	new file:   client/Fireport/Assets/Scripts/SetWinner.cs 	new file:   client/Fireport/Assets/Scripts/SetWinner.cs.meta 	new file:   client/Fireport/Assets/Scripts/WinnerRememberer.cs 	new file:   client/Fireport/Assets/Scripts/WinnerRememberer.cs.meta

---
+++ client/Fireport/Assets/Scripts/SetWinner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetWinner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () { 
+		UnityEngine.UI.Text text = GetComponent<UnityEngine.UI.Text>();
+		text.text = string.Format("A Winner is: {0}", WinnerRememberer.Winner);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
